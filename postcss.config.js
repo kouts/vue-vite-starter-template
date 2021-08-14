9e10dd532bdc991853f7ffdf1918698e370c4037
Kouts <koutsaftakis@gmail.com>
2021-08-14 11:31:53 +0300

    fix: fixed postcss removing used styles

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     require('autoprefixer'),
     IN_PRODUCTION &&
       require('@fullhuman/postcss-purgecss')({
-        content: ['./public/**/*.html', './src/**/*.vue'],
+        content: ['./**/*.html', './src/**/*.vue'],
         defaultExtractor(content) {
           const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
           return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
