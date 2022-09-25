9965c63412fc2e73d32802bd54d811995d46bd4b
Kouts <koutsaftakis@gmail.com>
2022-09-25 17:15:42 +0300

    fix: replaced logo with svg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="text-center">
-      <img alt="Vue logo" src="@/assets/logo.png" class="text-center" />
+      <img alt="Vue logo" src="@/assets/logo.svg" class="text-center" />
     </div>
     <hello-world msg="Welcome to Vue.js Vite starter template" />
   </div>
